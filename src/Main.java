8957c669e3732129d12a85bc3afa7e31d51fa392
behru <umarovb768@gmail.com>
2024-06-07 16:30:56 +0300

    аш

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.lang.Math;
 
 class FunctionCalculator {
     public static void main(String[] args) {
-        double[] xValues = {1.0, 2.0, 3.0, 4.0, 5.0}; // Пример значений x
+        double[] xValues = {1.0, 2.0, 3.0, 4.0, 5.0};
         System.out.println("Вычисление значений функции f(x) = e^x:");
 
         for (double x : xValues) {
-            double result = Math.exp(x); // Вычисление значения функции
+            double result = Math.exp(x);
             System.out.println("x = " + x + ", f(x) = " + result);
         }
     }
